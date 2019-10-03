e1a3a8f81c2b500daf2c7407712fb1bb7b8d82bd
Hadrien Mary <hadrien.mary@gmail.com>
2019-10-03 18:35:49 -0400

    autoformat 2 spaces

--- jupyter_archive/__init__.py
+++ jupyter_archive/__init__.py
@@ -4,11 +4,11 @@ from .handlers import ArchiveHandler
 
 # Jupyter Extension points
 def _jupyter_server_extension_paths():
-    return [{'module': 'jupyter_archive'}]
+  return [{'module': 'jupyter_archive'}]
 
 
 def load_jupyter_server_extension(nbapp):
-    base_url = url_path_join(nbapp.web_app.settings['base_url'], '/archive-download')
-    handlers = [(base_url, ArchiveHandler)]
-    nbapp.web_app.add_handlers('.*', handlers)
-    nbapp.log.info("jupyter_archive is enabled.")
+  base_url = url_path_join(nbapp.web_app.settings['base_url'], '/archive-download')
+  handlers = [(base_url, ArchiveHandler)]
+  nbapp.web_app.add_handlers('.*', handlers)
+  nbapp.log.info("jupyter_archive is enabled.")
--- jupyter_archive/handlers.py
+++ jupyter_archive/handlers.py
@@ -8,72 +8,72 @@ from notebook.base.handlers import IPythonHandler
 
 
 class ArchiveStream():
-    def __init__(self, handler):
-        self.handler = handler
-        self.position = 0
+  def __init__(self, handler):
+    self.handler = handler
+    self.position = 0
 
-    def write(self, data):
-        self.position += len(data)
-        self.handler.write(data)
+  def write(self, data):
+    self.position += len(data)
+    self.handler.write(data)
 
-    def tell(self):
-        return self.position
+  def tell(self):
+    return self.position
 
-    async def flush(self):
-        await self.handler.flush()
+  async def flush(self):
+    await self.handler.flush()
 
 
 def make_writer(handler, archive_format="zip"):
-    fileobj = ArchiveStream(handler)
+  fileobj = ArchiveStream(handler)
 
-    if archive_format == "zip":
-        archive_file = zipfile.ZipFile(fileobj, mode='w')
-        archive_file.add = archive_file.write
-    elif archive_format == "tgz":
-        archive_file = tarfile.open(fileobj=fileobj, mode='w|gz')
-    elif archive_format == "tbz":
-        archive_file = tarfile.open(fileobj=fileobj, mode='w|bz2')
-    elif archive_format == "txz":
-        archive_file = tarfile.open(fileobj=fileobj, mode='w|xz')
-    else:
-        raise ValueError("'{}' is not a valid archive format.".format(archive_format))
-    return archive_file
+  if archive_format == "zip":
+    archive_file = zipfile.ZipFile(fileobj, mode='w')
+    archive_file.add = archive_file.write
+  elif archive_format == "tgz":
+    archive_file = tarfile.open(fileobj=fileobj, mode='w|gz')
+  elif archive_format == "tbz":
+    archive_file = tarfile.open(fileobj=fileobj, mode='w|bz2')
+  elif archive_format == "txz":
+    archive_file = tarfile.open(fileobj=fileobj, mode='w|xz')
+  else:
+    raise ValueError("'{}' is not a valid archive format.".format(archive_format))
+  return archive_file
 
 
 class ArchiveHandler(IPythonHandler):
 
-    @web.authenticated
-    async def get(self):
+  @web.authenticated
+  async def get(self):
 
-        archive_path = self.get_argument('archivePath')
-        archive_token = self.get_argument('archiveToken')
-        archive_format = self.get_argument('archiveFormat', 'zip')
+    archive_path = self.get_argument('archivePath')
+    archive_token = self.get_argument('archiveToken')
+    archive_format = self.get_argument('archiveFormat', 'zip')
 
-        archive_path = pathlib.Path(archive_path)
-        archive_name = archive_path.name
-        archive_filename = archive_path.with_suffix(".{}".format(archive_format)).name
+    archive_path = pathlib.Path(archive_path)
+    archive_name = archive_path.name
+    archive_filename = archive_path.with_suffix(".{}".format(archive_format)).name
 
-        # We gonna send out event streams!
-        self.set_header('content-type', 'application/octet-stream')
-        self.set_header('cache-control', 'no-cache')
-        self.set_header('content-disposition',
-                        'attachment; filename={}'.format(archive_filename)
-                        )
+    # We gonna send out event streams!
+    self.set_header('content-type', 'application/octet-stream')
+    self.set_header('cache-control', 'no-cache')
+    self.set_header('content-disposition',
+                    'attachment; filename={}'.format(archive_filename)
+                    )
 
-        try:
-            self.log.info('Prepare {} for archiving and downloading.'.format(archive_filename))
-            archive_writer = make_writer(self, archive_format)
+    try:
+      self.log.info('Prepare {} for archiving and downloading.'.format(archive_filename))
+      archive_writer = make_writer(self, archive_format)
 
-            with archive_writer as writer:
-                for file_path in archive_path.rglob("*"):
-                    writer.add(file_path, file_path.relative_to(archive_path))
-                    await self.flush()
+      with archive_writer as writer:
+        for file_path in archive_path.rglob("*"):
+          writer.add(file_path, file_path.relative_to(archive_path))
+          await self.flush()
 
-        except iostream.StreamClosedError:
-            self.log.info('Downloading {} has been canceled by the client.'.format(archive_filename))
+    except iostream.StreamClosedError:
+      self.log.info('Downloading {} has been canceled by the client.'.format(archive_filename))
 
-        else:
-            self.set_cookie("archiveToken", archive_token)
-            self.log.info('Finished downloading {}.'.format(archive_filename))
+    else:
+      self.set_cookie("archiveToken", archive_token)
+      self.log.info('Finished downloading {}.'.format(archive_filename))
 
-        self.finish()
+    self.finish()
