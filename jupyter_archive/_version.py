18f0b757d95874bca42bf85266b6d96ca4d9570b
Hadrien Mary <hadrien.mary@gmail.com>
2019-10-06 14:04:31 -0400

    Bump to 0.3.3

--- jupyter_archive/_version.py
+++ jupyter_archive/_version.py
@@ -1,5 +1,5 @@
 # Copyright (c) Project Jupyter.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 3, 3, "dev0")
+version_info = (0, 3, 3)
 __version__ = ".".join(map(str, version_info))
