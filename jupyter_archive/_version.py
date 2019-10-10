2bddd73852e0491718b2e46a09db0189621924e9
Hadrien Mary <hadrien.mary@gmail.com>
2019-10-10 15:04:08 -0400

    Bump to dev

--- jupyter_archive/_version.py
+++ jupyter_archive/_version.py
@@ -1,5 +1,5 @@
 # Copyright (c) Project Jupyter.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 4, 3)
+version_info = (0, 4, 4, "dev")
 __version__ = ".".join(map(str, version_info))
