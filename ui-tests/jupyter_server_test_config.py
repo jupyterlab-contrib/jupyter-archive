4ee30afe4361570c860c253faef1dff5d6b4b73b
Frederic COLLONVAL <fcollonval@users.noreply.github.com>
2023-08-15 15:50:36 +0200

    Fix server setup for JupyterLab 3

--- ui-tests/jupyter_server_test_config.py
+++ ui-tests/jupyter_server_test_config.py
@@ -4,15 +4,30 @@
 opens the server to the world and provide access to JupyterLab
 JavaScript objects through the global window variable.
 """
-from jupyterlab.galata import configure_jupyter_server
+try:
+    # Available from JupyterLab 4 and later
+    from jupyterlab.galata import configure_jupyter_server
 
-configure_jupyter_server(c)
+    configure_jupyter_server(c)
 
-# Option specific for notebook v7+
-import jupyterlab
-from pathlib import Path
-c.JupyterNotebookApp.expose_app_in_browser = True
-c.LabServerApp.extra_labextensions_path = str(Path(jupyterlab.__file__).parent / "galata")
+    # Option specific for notebook v7+
+    import jupyterlab
+    from pathlib import Path
+    c.JupyterNotebookApp.expose_app_in_browser = True
+    c.LabServerApp.extra_labextensions_path = str(Path(jupyterlab.__file__).parent / "galata")
+
+except ImportError:
+    from tempfile import mkdtemp
+
+    c.ServerApp.port = 8888
+    c.ServerApp.port_retries = 0
+    c.ServerApp.open_browser = False
+
+    c.ServerApp.root_dir = mkdtemp(prefix='galata-test-')
+    c.ServerApp.token = ""
+    c.ServerApp.password = ""
+    c.ServerApp.disable_check_xsrf = True
+    c.LabApp.expose_app_in_browser = True
 
 # Uncomment to set server log level to debug level
 # c.ServerApp.log_level = "DEBUG"
